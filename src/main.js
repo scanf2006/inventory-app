5a9f63b0765891b0f14b3aca6fd73e848a6de47f
scanf2006 <scanf2006@gmail.com>
2026-03-28 14:58:25 -0400

    style(logo): add day of the week to current date display (v3.1.44)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const App = {
   Config: {
-    VERSION: "v3.1.43",
+    VERSION: "v3.1.44",
     SUPABASE_URL: "https://kutwhtcvhtbhbhhyqiop.supabase.co",
     SUPABASE_KEY:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dHdodGN2aHRiaGJoaHlxaW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NDE4OTUsImV4cCI6MjA4NjMxNzg5NX0.XhQ4m5SXV0GfmryV9iRQE9FEsND3HAep6c56VwPFcm4",
@@ -340,6 +340,7 @@ function initApp() {
   // UI Init
   document.getElementById("current-date").innerText =
     new Date().toLocaleDateString("en-US", {
+      weekday: "long",
       year: "numeric",
       month: "long",
       day: "numeric",
