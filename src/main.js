c0aeabc21e51cfce60d2c854128e699a24f71e74
scanf2006 <scanf2006@gmail.com>
2026-04-21 19:19:26 -0400

    style: restore close button size now that padding is added (v3.1.61)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const App = {
   Config: {
-    VERSION: "v3.1.60",
+    VERSION: "v3.1.61",
     ADMIN_PASSWORD: "9898",
     SUPABASE_URL: "https://kutwhtcvhtbhbhhyqiop.supabase.co",
     SUPABASE_KEY:
