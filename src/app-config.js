32cd1468a7f86aea7e3ee5a2ae8072058363ad36
scanf2006 <scanf2006@gmail.com>
2026-06-21 14:23:23 -0400

    Center odd mobile preview card and bump version to 3.8.41

--- src/app-config.js
+++ src/app-config.js
@@ -5,7 +5,7 @@
 window.App = window.App || {};
 
 App.Config = {
-  VERSION: "v3.8.40",
+  VERSION: "v3.8.41",
   ADMIN_PASSWORD: "9898",
   SUPABASE_URL: "https://kutwhtcvhtbhbhhyqiop.supabase.co",
   SUPABASE_KEY:
